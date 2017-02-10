f0cca12d6606124b087b6f821e5486e02d4ea583
Michael Lindon <michael.s.lindon@gmail.com>
2017-02-10 16:07:10 -0500

    indentation

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-		 [net.mikera/core.matrix "0.57.0"]
+                 [net.mikera/core.matrix "0.57.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
-		 [org.clojure/data.avl "0.0.17"]
+                 [org.clojure/data.avl "0.0.17"]
                  [incanter/incanter-core "1.9.1"]
-		 [org.clojure/math.combinatorics "0.1.4"]])
+                 [org.clojure/math.combinatorics "0.1.4"]])
