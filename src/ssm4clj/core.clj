cf0df4a934d9993252be4603d502ecea72b0751f
Michael Lindon <michael.s.lindon@gmail.com>
2017-08-11 11:03:55 -0400

    added approximations

--- src/ssm4clj/core.clj
+++ src/ssm4clj/core.clj
@@ -20,6 +20,13 @@
               (* 0.125 em2dl q (pow d 2) (pow (+ -2 (* d l)) 2))
               (/ (* q (+ 3 (* em2dl (+ -3 (* -2 d l (+ -5 (* d l (+ 11 (* d l (+ -6 (* d l))))))))))) (* 16 l))]])))
 
+(defn approx-corr [ls d]
+  (let [l (/ (sqrt 5) ls)
+        q (/ (* 16 (pow l 5)) 3)]
+    (matrix [[0 0 0] [0 0 0] [0 0 (* q d)]])))
+
+(def approx-reverse-corr approx-corr)
+
 (defn reverse-corr
   "Computes reverse-correlation matrix for length scale ls and delay d"
   [ls d]
@@ -70,6 +77,15 @@
               (* emdl d l l (+ -3 (* d l)))
               (* 0.5 emdl (+ 2 (* -4 d l) (* d d l l)))]])))
 
+(defn approx-regression
+  "Returns a regression matrix for length scale ls and delay d"
+  [ls d]
+  (let [l (/ (sqrt 5) ls)]
+    (matrix [[1 d 0] [0 1 d]
+             [(* d l l l -1)
+              (* d l l -3)
+              (- 1 (* 3 d l))]])))
+
 (defn reverse-regression
   "Returns a regression matrix for length scale ls and delay d"
   [ls d]
@@ -85,6 +101,15 @@
              [(* 0.5 emdl d l l l (+ -2 (* d l)))
               (* -1 emdl d l l (+ -3 (* d l)))
               (* 0.5 emdl (+ 2 (* -4 d l) (* d d l l)))]])))
+
+(defn approx-reverse-regression
+  "Returns a regression matrix for length scale ls and delay d"
+  [ls d]
+  (let [l (/ (sqrt 5) ls)]
+    (matrix [[1 (negate d) 0] [ 0 1 (negate d)]
+             [(* d l l l -1)
+              (negate (* d l l -3))
+              (- 1 (* 3 d l))]])))
 
 (defn statcorr
   "Returns the stationary correlation matrix for length scale ls"
